cb54a78b0e6d450ca406cd6739c911a061870755
galambalazs <galambalazs@yahoo.co.uk>
2011-05-29 20:46:26 +0200

    simplified apejs.run, also only run apejs.before if it's callable

--- war/WEB-INF/modules/apejs.js
+++ war/WEB-INF/modules/apejs.js
@@ -7,38 +7,37 @@ var apejs = {
         var httpMethod = request.getMethod().toLowerCase();
 
         // before running the http verb method run the before handler
-        if(this.before)
+        if (typeof this.before == "function") {
             this.before(request, response);
-
-        var matchedUrl = false;
-        for(var i in this.urls) {
-            var regex = "^"+i+"/?$";
-            var matches = path.match(new RegExp(regex));
-            if(matches && matches.length) { // matched!
-                this.urls[i][httpMethod](request, response, matches);
-                matchedUrl = true;
-                break; // we found it, stop searching
-            }
         }
 
-        if(!matchedUrl) { // try accessing static content inside APP_PATH/public
-            try {
-                // FIXME - this is really ugly - find other way to get servlet Context
-                var mimeType = ApeServlet.CONFIG.getServletContext().getMimeType(path);
-                var resPath = ApeServlet.APP_PATH+"/public"+path;
-                var res = new File(resPath);
-                // create an array of bytes as big as the file
-                var b = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, res.length()); 
-                var fileInputStream = new FileInputStream(res);
-                // read file contents into byte array
-                fileInputStream.read(b);
-
-                response.setContentType(mimeType);
-                response.getOutputStream().write(b);
-            } catch (e) {
-                // send 404
-                response.sendError(response.SC_NOT_FOUND);
+        for (var i in this.urls) {
+            var regex = "^"+i+"/?$";
+            var matches = path.match(new RegExp(regex));
+            if (matches && matches.length) {
+                this.urls[i][httpMethod](request, response, matches);
+                return; // we found it, stop searching
             }
+        }
+        
+        // there was no matching url
+        // try accessing static content inside APP_PATH/public
+        try {
+            // FIXME - this is really ugly - find other way to get servlet Context
+            var mimeType = ApeServlet.CONFIG.getServletContext().getMimeType(path);
+            var resPath = ApeServlet.APP_PATH+"/public"+path;
+            var res = new File(resPath);
+            // create an array of bytes as big as the file
+            var b = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, res.length()); 
+            var fileInputStream = new FileInputStream(res);
+            // read file contents into byte array
+            fileInputStream.read(b);
+
+            response.setContentType(mimeType);
+            response.getOutputStream().write(b);
+        } catch (e) {
+            // send 404
+            response.sendError(response.SC_NOT_FOUND);
         }
     }
 };
